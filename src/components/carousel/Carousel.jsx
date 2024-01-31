048d5f51b62bc91a03ad3dec5e9360541ffe502c
Alan Luna <85804890+AlanLuna-00@users.noreply.github.com>
2024-01-31 19:36:18 -0300

    Refactor carousel component and update client logos

--- src/components/carousel/Carousel.jsx
+++ src/components/carousel/Carousel.jsx
@@ -1,68 +1,78 @@
-import { Navigation, Pagination, Scrollbar, A11y } from 'swiper/modules';
-import butacas from '../../../public/images/filas-asientos-rojos-teatro.jpg';
-import { Swiper, SwiperSlide } from 'swiper/react';
-import './Carousel.css'
+import { Navigation, Pagination, Scrollbar, A11y } from "swiper/modules";
+import butacas from "../../../public/images/filas-asientos-rojos-teatro.jpg";
+import carrousel1 from "../../../public/images/carrousel1.jpeg";
+import carrousel2 from "../../../public/images/carrousel2.jpeg";
+import carrousel3 from "../../../public/images/carrousel3.jpeg";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "./Carousel.css";
 
 // Import Swiper styles
-import 'swiper/css';
-import 'swiper/css/navigation';
-import 'swiper/css/pagination';
-import 'swiper/css/scrollbar';
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import "swiper/css/scrollbar";
 
 const Carousel = () => {
   return (
-    <div className='carousel-container'>
-
-    <Swiper
-      // install Swiper modules
-      className='carousel'
-      modules={[Navigation, Pagination, Scrollbar, A11y]}
-      // spaceBetween={50}
-      slidesPerView={1}
-      navigation
-      pagination={{ clickable: true }}
-      scrollbar={{ draggable: true }}
-      onSwiper={(swiper) => console.log(swiper)}
-      onSlideChange={() => console.log('slide change')}
-      breakpoints={{
-        // Configuración para dispositivos móviles con ancho máximo de 700px
-        700: {
-          slidesPerView: 1,
-          // spaceBetween: 300,
-        },
-        // Configuración para tabletas con ancho máximo de 1024px
-        1024: {
-          slidesPerView: 1,
-          // spaceBetween: 400,
-        },
-        // Configuración para pantallas más grandes
-        1200: {
-          slidesPerView: 1,
-          // spaceBetween: 200,
-        },
-      }}
-    >
-      <SwiperSlide>
-        <img className="carousel-img" src={butacas} alt="sillones laterales" />
-      </SwiperSlide>
-      <SwiperSlide>
-        <img className="carousel-img" src={butacas} alt="sillones laterales" />
-      </SwiperSlide>
-      <SwiperSlide>
-        <img className="carousel-img" src={butacas} alt="sillones frontales" />
-      </SwiperSlide>
-      <SwiperSlide>
-        <img className="carousel-img" src={butacas} alt="sillones frontales" />
-      </SwiperSlide>
-      <SwiperSlide>
-        <img className="carousel-img" src={butacas} alt="butacas rojas" />
-      </SwiperSlide>
-      <SwiperSlide>
-        <img className="carousel-img" src={butacas} alt="cojines" />
-      </SwiperSlide>
-      ...
-    </Swiper>
-
+    <div className="carousel-container">
+      <Swiper
+        // install Swiper modules
+        className="carousel"
+        modules={[Navigation, Pagination, Scrollbar, A11y]}
+        // spaceBetween={50}
+        slidesPerView={1}
+        navigation
+        pagination={{ clickable: true }}
+        scrollbar={{ draggable: true }}
+        onSwiper={(swiper) => console.log(swiper)}
+        onSlideChange={() => console.log("slide change")}
+        breakpoints={{
+          // Configuración para dispositivos móviles con ancho máximo de 700px
+          700: {
+            slidesPerView: 1,
+            // spaceBetween: 300,
+          },
+          // Configuración para tabletas con ancho máximo de 1024px
+          1024: {
+            slidesPerView: 1,
+            // spaceBetween: 400,
+          },
+          // Configuración para pantallas más grandes
+          1200: {
+            slidesPerView: 1,
+            // spaceBetween: 200,
+          },
+        }}
+      >
+        <SwiperSlide>
+          <img
+            className="carousel-img"
+            src={carrousel1}
+            alt="sillones laterales"
+          />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img
+            className="carousel-img"
+            src={carrousel2}
+            alt="sillones laterales"
+          />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img
+            className="carousel-img"
+            src={carrousel3}
+            alt="sillones frontales"
+          />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img
+            className="carousel-img"
+            src={butacas}
+            alt="sillones frontales"
+          />
+        </SwiperSlide>
+      </Swiper>
     </div>
   );
 };
--- src/components/clientes/Clientes.jsx
+++ src/components/clientes/Clientes.jsx
@@ -1,27 +1,31 @@
-import React from 'react'
-import './Clientes.css'
-// Logos >>> 
-import buenosAires from '../../../public/images/buenos-aires-ciudad-logo.png'
+import React from "react";
+import "./Clientes.css";
+// Logos >>>
+import buenosAires from "../../../public/images/buenos-aires-ciudad-logo.png";
+import lacosta from "../../assets/clientes/lacosta.jpeg";
+import granRex from "../../assets/clientes/granrex.png";
+import cineMultiplex from "../../assets/clientes/ multiplex.jpeg";
+import gba from "../../assets/clientes/gba.webp";
+import lyf from "../../assets/clientes/lyf.png";
+import regi from "../../assets/clientes/regi.png";
 
 export const Clientes = () => {
-    const clientes = [
-        {nombre: 'Buenos Aires', logo: buenosAires},
-        {nombre: 'Buenos Aires', logo: buenosAires},
-        {nombre: 'Buenos Aires', logo: buenosAires},
-        {nombre: 'Buenos Aires', logo: buenosAires},
-        {nombre: 'Buenos Aires', logo: buenosAires},
-        {nombre: 'Buenos Aires', logo: buenosAires},
-        {nombre: 'Buenos Aires', logo: buenosAires},
-        {nombre: 'Buenos Aires', logo: buenosAires},
-    ]
+  const clientes = [
+    { nombre: "CABA", logo: buenosAires },
+    { nombre: "La Costa", logo: lacosta },
+    { nombre: "Gran Rex", logo: granRex },
+    { nombre: "Cine Multiplex", logo: cineMultiplex },
+    { nombre: "GBA", logo: gba },
+    { nombre: "Luz y Fuerzas", logo: lyf },
+    { nombre: "Teatro Regina", logo: regi },
+    // { nombre: "Buenos Aires", logo: buenosAires },
+  ];
 
   return (
-    <div className='clientes-container'>
-        {
-            clientes.map(({nombre, logo}, index) => (
-                <img key={index} src={logo} alt={nombre} />
-            ))
-        }
+    <div className="clientes-container">
+      {clientes.map(({ nombre, logo }, index) => (
+        <img key={index} src={logo} alt={nombre} />
+      ))}
     </div>
-  )
-}
+  );
+};
