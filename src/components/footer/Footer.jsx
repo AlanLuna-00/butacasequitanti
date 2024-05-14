bae075decfacef97188b91fcff2f7b2d717159a4
Alan Luna <alan.luna.desarrollo@gmail.com>
2024-05-13 21:46:55 -0300

    chore: Actualizar estilos y funcionalidad del footer y agregar enlaces de navegación

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,47 +1,63 @@
 import "./Footer.css";
 
 export const Footer = () => {
+  const handleLinkClick = (targetId) => {
+    const targetElement = document.getElementById(targetId);
+
+    if (targetElement) {
+      const navbarHeight = document.querySelector(".top-nav").offsetHeight;
+      window.scrollTo({
+        top: targetElement.offsetTop - navbarHeight,
+        behavior: "smooth",
+      });
+    }
+  };
+
+  const isMobile = window.innerWidth <= 768;
   return (
-    <footer className="footer-distributed">
-      <div className="footer-left">
-        <h3>
-          Butacas <span>Equitanti</span>
-        </h3>
+    <footer>
+      <div className="top">
+        <div className="pages">
+          <ul>
+            <h3>Butacas Equitanti</h3>
+            <li onClick={() => handleLinkClick("inicio")}>Inicio</li>
 
-        {/* <p className="footer-links">
-					<a href="#" className="link-1"
-					onClick={() => handleLinkClick("trabajos-anteriores")}
-					>Inicio</a>
-					
-					<a href="#">Blog</a>
-				
-					<a href="#">Pricing</a>
-				
-					<a href="#">About</a>
-					
-					<a href="#">Faq</a>
-					
-					<a href="#">Contact</a>
-				</p> */}
+            <li onClick={() => handleLinkClick("descripcion")}>
+              Sobre nosotros
+            </li>
 
-        {/* <p className="footer-company-name">Company Name © 2015</p> */}
-      </div>
+            <li onClick={() => handleLinkClick("trabajos-anteriores")}>
+              Trabajos anteriores
+            </li>
 
-      <div className="footer-center">
-        <div>
-          <i className="fa fa-map-marker"></i>
-          <p>
-            Ramos Mejia <span>La Matanza, Buenos Aires</span>
-          </p>
+            <li onClick={() => handleLinkClick("caracteristicas")}>
+              Caracteristicas
+            </li>
+
+            <li onClick={() => handleLinkClick("experiencia")}>
+              Nuestra experiencia
+            </li>
+
+            <li onClick={() => handleLinkClick("nuestros-clientes")}>
+              Nuestros clientes
+            </li>
+          </ul>
+
+          <ul className="footer-compania">
+            <h3>Nuestra compania</h3>
+            <li>
+              <span>
+                Equipamiento para cines y teatros desde 1942.
+                <br />
+                Calidad y servicio líderes.
+                <br />
+                Tapicería, butacas y más para disfrutar del arte
+              </span>
+            </li>
+          </ul>
         </div>
-
-        {/* <div>
-					<i className="fa fa-phone"></i>
-					<p>+1.555.555.5555</p>
-				</div> */}
-
-        <div>
-          <i className="fa fa-envelope"></i>
+        <div className="newsletter">
+          <h3>Contactanos!</h3>
           <p>
             <a
               href="mailto:butacasequitanti@hotmail.com
@@ -51,52 +67,30 @@ export const Footer = () => {
             </a>
           </p>
         </div>
-        <div>
-          <a
-            href="https://www.instagram.com/butacasequitanti/"
-            target="_blank"
-            rel="noreferrer"
-            style={{
-              textDecoration: "none",
-              color: "white",
-            }}
-          >
-            <img
-              src="https://upload.wikimedia.org/wikipedia/commons/9/95/Instagram_logo_2022.svg"
-              alt="instagram logo"
-              width="30px"
-              height="30px"
-              style={
-                // media query
-                window.matchMedia("(max-width: 600px)").matches
-                  ? { margin: "0" }
-                  : { margin: "0 0 0 30px" }
-              }
-            />
-          </a>
+      </div>
+      <div className="social">
+        <a
+          href="https://www.instagram.com/butacasequitanti?igsh=d3F2YmVuOXczM24x"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <i className="fab fa-instagram"></i>
+        </a>
+        <i className="fab fa-facebook"></i>
+        <a
+          href="https://maps.app.goo.gl/9rz4Eihv8Ggm1Etb8"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <i className="fa fa-map-pin" aria-hidden="true"></i>
+        </a>
+      </div>
+      <div className="info">
+        <div className="legal">
+          <span>Ramos Mejia, La Matanza, Buenos Aires </span>
         </div>
+        <div className="copyright">2024 Copyright &copy; Butacas Equitanti</div>
       </div>
-
-      <div className="footer-right">
-        <p className="footer-company-about">
-          <span>Nuestra compania</span>
-          Equipamiento para cines y teatros desde 1942. Calidad y servicio
-          líderes. Tapicería, butacas y más para disfrutar del arte.
-        </p>
-
-        {/* <div className="footer-icons">
-
-					<a href="#"><i className="fa fa-facebook"></i></a>
-					<a href="#"><i className="fa fa-twitter"></i></a>
-					<a href="#"><i className="fa fa-linkedin"></i></a>
-					<a href="#"><i className="fa fa-github"></i></a>
-
-				</div> */}
-      </div>
-
-      <p className="copyright">
-        Butacas Equitanti &copy; Todos los derechos reservados - 2024
-      </p>
     </footer>
   );
 };
